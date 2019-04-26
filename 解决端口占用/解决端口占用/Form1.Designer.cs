0865172dd4164a44d2958dad957c1d74f267a714
XiaomuSoftware <44538191+XiaomuSoftware@users.noreply.github.com>
2019-04-26 10:57:25 +0800

    Add files via upload

---
+++ 解决端口占用/解决端口占用/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace 解决端口占用
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.check = new System.Windows.Forms.Button();
+            this.fankui = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(74, 26);
+            this.port.Margin = new System.Windows.Forms.Padding(4);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(127, 26);
+            this.port.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(17, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "端口:";
+            // 
+            // check
+            // 
+            this.check.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.check.Location = new System.Drawing.Point(226, 24);
+            this.check.Margin = new System.Windows.Forms.Padding(4);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(116, 28);
+            this.check.TabIndex = 2;
+            this.check.Text = "查询端口占用";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // fankui
+            // 
+            this.fankui.AutoSize = true;
+            this.fankui.Location = new System.Drawing.Point(262, 70);
+            this.fankui.Name = "fankui";
+            this.fankui.Size = new System.Drawing.Size(80, 16);
+            this.fankui.TabIndex = 3;
+            this.fankui.TabStop = true;
+            this.fankui.Text = "反馈@小木";
+            this.fankui.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.fankui_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(367, 101);
+            this.Controls.Add(this.fankui);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.port);
+            this.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "解决端口占用";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.LinkLabel fankui;
+    }
+}
+
---
+++ 解决端口占用/解决端口占用/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace 解决端口占用
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string cmd(string strInput)
+        {
+            // 创建进程
+            Process p = new Process();
+            //设置要启动的应用程序
+            p.StartInfo.FileName = "cmd.exe";
+            //是否使用操作系统shell启动
+            p.StartInfo.UseShellExecute = false;
+            // 接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardInput = true;
+            //输出信息
+            p.StartInfo.RedirectStandardOutput = true;
+            // 输出错误
+            p.StartInfo.RedirectStandardError = true;
+            //不显示程序窗口
+            p.StartInfo.CreateNoWindow = true;
+            //启动程序
+            p.Start();
+            //向cmd窗口发送输入信息
+            p.StandardInput.WriteLine(strInput + "&exit");
+            p.StandardInput.AutoFlush = true;
+            //获取输出信息
+            string strOuput = p.StandardOutput.ReadToEnd();
+            //等待程序执行完退出进程
+            p.WaitForExit();
+            p.Close();
+            return strOuput;
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 获取端口
+                string port = this.port.Text;
+                // 判断是否输入
+                if (port == "")
+                {
+                    MessageBox.Show("请输入端口", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+                    return;
+                }
+                else
+                {
+                    string strInput = "netstat -aon|findstr " + port;
+                    string strOuput = cmd(strInput);
+                    // 判断是否有返回值
+                    if (strOuput == "")
+                    {
+                        MessageBox.Show("该端口未被占用");
+                        return;
+                    }
+                    else
+                    {
+                        int i = strOuput.IndexOf('&');
+                        strOuput = strOuput.Substring(i);
+                        // 查找数字
+                        MatchCollection matches = Regex.Matches(strOuput, "[0-9]+");
+                        // 定义list存放遍历结果
+                        List<string> list = new List<string>();
+                        // 遍历
+                        foreach (Match item in matches)
+                        {
+                            //item.Value表示本次提取到的字符串
+                            list.Add(item.Value);
+                        }
+                        // list转换成数组
+                        string[] arrs = list.ToArray();
+                        // 判断是否有返回值
+                        if (arrs == null)
+                        {
+                            MessageBox.Show("该端口未被占用");
+                            return;
+                        }
+                        else
+                        {
+                            // 获取第一个pid
+                            string pid = arrs[10];
+                            // 查询第一个pid是什么软件的进程
+                            strInput = "tasklist|findstr " + pid;
+                            strOuput = cmd(strInput);
+                            Regex reg = new Regex(@"\w+\.[e][x][e]");
+                            Match match = reg.Match(strOuput);
+                            string exe = "";
+                            if (match.Success)
+                            {
+                                exe = match.Value;
+                            }
+                            else
+                            {
+                                exe = "没查找到进程名";
+                            }
+                            //MessageBox.Show("占用此端口的进程为:  "+ exe + "\r\n\r\n是否关闭该进程释放端口?", "提示", MessageBoxButtons.OKCancel);
+                            DialogResult dr = MessageBox.Show("占用此端口的进程为:  " + exe + "\r\n\r\n是否关闭该进程释放端口?", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+                            if (dr == DialogResult.OK)
+                            {
+                                //点确定:  关闭该进程
+                                strInput = "taskkill /f /t /im " + exe;
+                                strOuput = cmd(strInput);
+                                reg = new Regex(@"[S][U][C][C][E][S][S]");
+                                match = reg.Match(strOuput);
+                                if (match.Success)
+                                {
+                                    MessageBox.Show("成功关闭" + exe + "进程", "提示");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("未关闭" + exe + "进程\r\n\r\n可能的原因如下:\r\n1：进程已经被关闭。\r\n2：进程已经未找到。\r\n3：没有权限，尝试管理员运行。", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                                }
+                            }
+                            else
+                            {
+                                //点取消的代码 
+                                return;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show("请输入正确的端口", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            }
+
+            
+            
+        }
+
+        private void fankui_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // 调用默认浏览器发起qq对话
+            Process.Start("http://wpa.qq.com/msgrd?v=3&uin=451508532&site=qq&menu=yes");
+        }
+    }
+}
